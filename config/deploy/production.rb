19b585cc0dc913353af12aff442ae7f814a02afd
Tony Bruess <tony@oc.tc>
2016-01-20 17:38:31 -0800

    Add new production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@
 # These servers get deployed to in the production stage
 
 server '50.16.44.122', roles: %w{app db}
+server '54.146.132.80', roles: %w{app db}
 
 # Also deploy all the staging servers
 load File.expand_path('staging.rb', File.dirname(__FILE__))
